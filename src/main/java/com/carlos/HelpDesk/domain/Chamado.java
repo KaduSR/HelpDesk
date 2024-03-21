8d9959b7de2da3fb779181dae977d7f066bc7edf
KaduSR <carlos847@gmail.com>
2024-03-21 20:13:27 -0300

    Anotacoes JPA para classes de dominio

--- src/main/java/com/carlos/HelpDesk/domain/Chamado.java
+++ src/main/java/com/carlos/HelpDesk/domain/Chamado.java
@@ -1,22 +1,49 @@
 package com.carlos.HelpDesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import com.carlos.HelpDesk.domain.enums.Prioridade;
 import com.carlos.HelpDesk.domain.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 import java.util.Objects;
 
-public class Chamado {
 
+
+
+@Entity
+public class Chamado  implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataAbertura = LocalDate.now();
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
+
+
     private Prioridade prioridade;
     private Status status;
     private String titulo;
     private String obervacoes;
 
+    @ManyToOne
+    @JoinColumn (name = "tecnico_id")
     private Tecnico tecnico;
+
+    @ManyToOne
+    @JoinColumn (name = "cliente_id")
     private Cliente cliente;
     public Chamado() {
         super();
